7cdac649a2735eb799b90b1febccdb8aa759fbb0
Josimar16 <josimarjr479@gmail.com>
2020-12-14 20:43:14 -0300

    :tada: resolvido problema de run do node-dev

--- src/modules/customers/infra/http/controller/CustomersController.ts
+++ src/modules/customers/infra/http/controller/CustomersController.ts
@@ -6,6 +6,7 @@ import { container } from 'tsyringe';
 
 export default class CustomersController {
   public async create(request: Request, response: Response): Promise<Response> {
-    // TODO
+    const { name, email } = request.body;
+    const customer = await CreateCustomerService();
   }
 }
